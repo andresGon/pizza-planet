a83311021d5f60f665c265425a49ecb9a7c0feb6
andres mauricio gonzalez <andres.gonzalez@cluvi.com>
2023-07-28 09:47:16 -0500

    rutas nombradas

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1 class="site_tittle">
+        <RouterLink :to="{name: 'home'}">== Pizza Planet ==</RouterLink>
+    </h1>
+    <nav>
+        <RouterLink class="link" :to="{name: 'home'}">Home</RouterLink>
+        <RouterLink class="link" :to="{name: 'menuLink'}">Menu</RouterLink>
+        <RouterLink class="link" :to="{name: 'aboutLink'}">About</RouterLink>
+        <RouterLink class="link" :to="{name: 'adminLink'}">Admin</RouterLink>
+    </nav>
+    <h2>The #1 Place for pizza</h2>
+  </header>
+</template>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Menu from '../views/MenuView.vue'
+import Admin from '../views/AdminView.vue'
+import About from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/menu',
+      name: 'menuLink',
+      component: Menu
+    },
+    {
+      path: '/admin',
+      name: 'adminLink',
+      component: Admin,
+    },
+    {
+      path: '/about',
+      name: 'aboutLink',
+      component: About
     },
     {
       path: '/about',
