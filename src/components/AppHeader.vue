80a1e77990bcb0441f5647ade3cbcb38959894e3
andres mauricio gonzalez <andres.gonzalez@cluvi.com>
2023-08-06 14:35:56 -0500

    menu maquetacion

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <h1 class="site_tittle">
+    <h1 class="site_title">
         <RouterLink :to="{name: 'home'}">== Pizza Planet ==</RouterLink>
     </h1>
     <nav>
@@ -13,3 +13,29 @@
   </header>
 </template>
 
+<style scoped>
+.site_title{
+  font-size: 3.6rem;
+  color: aliceblue;
+  transform: rotate(-8deg);
+  margin-bottom: 2rem;
+}
+
+header{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem 0;
+  color: aliceblue;
+  background: url("../assets/images/main-bg.jpg") no-repeat;
+  height: 650px;
+  background-size: cover;
+  background-position: center;
+  text-shadow: 1px 1px 3px rgb(20,20,20);
+}
+h2{
+  margin: 2rem 0;
+  color: antiquewhite;
+  font-size: 2rem;
+}
+</style>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,3 +1,84 @@
 <template>
-    <h3>Menu</h3>
+    <div class="menu_wrapper">
+        <div class="menu">
+            <h3>== Authentic handmade piza ==</h3>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <strong>= Margheria =</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <small>
+                                A delicious tomato-based pizza topped whith mozzarella
+                            </small>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>9"</td>
+                        <td>$7.95</td>
+                        <td>
+                            <button type="button">&#43</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="basket">
+            <h3>= Basket = </h3>
+            <div>
+                <table>
+                    <tr>
+                        <td>
+                            <button class="quantity_btn" type="button">&#8722</button>
+                            <span>1</span>
+                            <button class="quantity_btn" type="button">&#43</button>
+                        </td>
+                        <td>Margeria 9"</td>
+                        <td>$7.95</td>
+                    </tr>
+                </table>
+                <p>Order total: $87</p>
+                <button>Place order</button>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+h3{
+    text-align: center;
+}
+/* mobile layout */
+.menu_wrapper{
+    display: flex;
+    flex-direction: column;
+    color: rgb(80,96,112);
+}
+.menu,.basket{
+    background: #faf1e2;
+    border-radius: 3px;
+    height: 100vh;
+    padding: 1rem;
+}
+.quantity_btn{
+    border: none;
+    padding: 0.4rem;
+}
+@media (min-width: 900px) {
+    .menu_wrapper{
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    .menu{
+        flex: 2;
+        border-right: 1px solid rgb(202, 202, 202);
+    }
+    .basket{
+        flex: 1;
+    }
+    
+}
+</style>
