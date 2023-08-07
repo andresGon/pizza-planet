bfb8ff6537fc6d9b2eaf1486e60f65f4e33f5387
andres mauricio gonzalez <andres.gonzalez@cluvi.com>
2023-08-07 12:26:30 -0500

    end of project ui

---
+++ src/components/admin/NewPizza.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="admin_section">
+        <header class="admin_section_header">
+            <h3>Add new pizza</h3>
+        </header>
+            <form>
+                <div class="form_group">
+                    <label for="name">Name</label>
+                    <input type="text" id="name">
+                </div>
+                <div class="form_group">
+                    <label for="description">Description</label>
+                    <textarea rows="5" id="description"></textarea>
+                </div>
+                <p>
+                    <strong>Option 1:</strong>
+                </p>
+                <div class="form_group">
+                    <label for="size1">Size ("")</label>
+                    <input type="text" id="size1">
+                </div>
+                <div class="form_group">
+                    <label for="price1">price</label>
+                    <input type="text" id="price1">
+                </div>
+
+                <p>
+                    <strong>Option 2:</strong>
+                </p>
+                <div class="form_group">
+                    <label for="size2">Size ("")</label>
+                    <input type="text" id="size2">
+                </div>
+                <div class="form_group">
+                    <label for="price2">price</label>
+                    <input type="text" id="price2">
+                </div>
+                <div class="form_group">
+                    <button>Add</button>
+                </div>
+            </form>
+        
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,48 @@
 <script setup>
+import { RouterLink } from 'vue-router';
+
 
 </script>
 
 <template>
-  <main>
-    <h3>Home</h3>
+  <main class="hot">
+    <div class="hot_text">
+      <h3>Hot out the oven!</h3>
+      <p class="hot_pizza_name">Vegetarian Hot Pizza</p>
+      <p class="hot_pizza_description">
+        Our signature vegetarian, but with a kick...
+      </p>
+      <RouterLink :to="{name: 'menuLink'}">Let's order</RouterLink>
+    </div>
+    <img src="../assets/images/fire.jpg" alt="pizza oven image" class="hot_image">
   </main>
 </template>
+
+<style scoped>
+  .hot{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .hot_image, .hot_text{
+    flex: 1;
+    padding: 2rem;
+    text-align: center;
+  }
+
+  .hot_text h3{
+    color: #bb2118;
+  }
+  .hot_pizza_name{
+    font-size: 1.8rem;
+  }
+  .hot_pizza_description{
+    font-style: italic;
+    font-weight: 300;
+  }
+  @media (min-width: 900px) {
+    .hot{
+      flex-direction: row;
+    }
+  }
+</style>
